84bb27d1f74239c8de04d9a758a8dd236ef03104
ostapmk <ostapmk@gmail.com>
2019-10-16 21:49:38 +0300

    Skeletons for tests

---
+++ Tests/TestBigNum.cpp
@@ -0,0 +1,7 @@
+#include <BigNum.hpp>
+
+#include "catch.hpp"
+
+TEST_CASE("Big numbers test", "[bignum]") {
+
+}
---
+++ Tests/TestEllipticCurves.cpp
@@ -0,0 +1,7 @@
+#include <EllipticCurves.hpp>
+
+#include "catch.hpp"
+
+TEST_CASE("Elliptic curves test", "[curves]") {
+  
+}
