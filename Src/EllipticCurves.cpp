ea6ba8e137c50009ee66611a65660407e922325d
SophyaTs <sophya.tsubin@gmail.com>
2019-12-01 17:39:12 +0200

    Code appereance improved, added minor change in fucntion EllipticCurves::add

--- Src/EllipticCurves.cpp
+++ Src/EllipticCurves.cpp
@@ -1,82 +1,76 @@
 #include <EllipticCurves.hpp>
 
 namespace lab {
-	EllipticCurve::EllipticCurve(Field* f, const BigNum& a, const BigNum& b): _f(f),_a(a),_b(b){}
 
-	bool operator==(const EllipticCurve& left, const EllipticCurve& right) {
-		return (*left._f == *right._f) && (left._a == right._a) && (left._b == right._b);
-	}
+EllipticCurve::EllipticCurve(Field* f, const BigNum& a, const BigNum& b): _f(f),_a(a),_b(b){}
 
-	bool operator!=(const EllipticCurve& left, const EllipticCurve& right) {
-		return !(left == right);
-	}
+bool operator==(const EllipticCurve& left, const EllipticCurve& right) {
+    return (*left._f == *right._f) && (left._a == right._a) && (left._b == right._b);
+}
 
-    bool EllipticCurve::contains(const Point& p) const {
-        if (multiply(p.y, p.y, _f->generator)
-            == lab::add(multiply(multiply(p.x, p.x, _f->generator), p.x, _f->generator), lab::add(multiply(_a, p.x, _f->generator), _b, _f->generator), _f->generator))
-            return true;
-        else 
-            return false;
-    }
+bool operator!=(const EllipticCurve& left, const EllipticCurve& right) {
+    return !(left == right);
+}
 
-	Point EllipticCurve::inverted(const Point& p) {
-		return { p.x,_f->generator - p.y };
-	}
+bool EllipticCurve::contains(const Point& p) const {
+    if (multiply(p.y, p.y, _f->generator)
+           == lab::add(multiply(multiply(p.x, p.x, _f->generator), p.x, _f->generator), lab::add(multiply(_a, p.x, _f->generator), _b, _f->generator), _f->generator))
+       return true;
+    else 
+       return false;
+ }
 
-	Point EllipticCurve::add(const Point& first, const Point& second) {
-		if ((first.x == second.x && first.y != second.y)
-			|| (first == second && first.y == 0_bn))
-		{
-			return neutral;
-		} else {
-			BigNum tmp1;
-			BigNum tmp2;
-			BigNum m;
-			if (first.x != second.x) {
-				tmp1 = second.y >= first.y ? 
-					subtract(second.y, first.y, _f->generator) 
-					: lab::add(subtract(_f->generator, first.y, _f->generator), second.y, _f->generator); //y2-y1
-               // std::cout << tmp1 << std::endl;
+Point EllipticCurve::inverted(const Point& p) {
+    return { p.x,_f->generator - p.y };
+}
 
-				tmp2 = second.x >= first.x ? 
-					subtract(second.x, first.x, _f->generator) 
-					: lab::add(subtract(_f->generator, first.x, _f->generator), second.x, _f->generator); //x2-x1
-                //std::cout << tmp2 << std::endl;
-			}
-			else {
-				tmp1 = multiply(first.x, first.x, _f->generator); //x1^2
-				tmp1 = multiply(3_bn, tmp1, _f->generator); //3*x1^2
-				tmp1 = lab::add(tmp1, _a, _f->generator);//3*x1^2 + A
-				tmp2 = multiply(2_bn, first.y, _f->generator);//2y1
-			}
-			m = multiply(tmp1,lab::inverted(tmp2,_f->generator, BigNum::InversionPolicy::Fermat),_f->generator); //tmp1 / tmp2
-            //std::cout << m << std::endl;
+Point EllipticCurve::add(const Point& first, const Point& second) {
+    if (first == neutral || second == neutral)
+        return first == neutral ? second : first;
+    
+    if ((first.x == second.x && first.y != second.y)
+        || (first == second && first.y == 0_bn))
+    {
+        return neutral;
+    } else {
+        BigNum tmp1;
+        BigNum tmp2;
+        BigNum m;
+        if (first.x != second.x) {
+        tmp1 = second.y >= first.y ? 
+                subtract(second.y, first.y, _f->generator) 
+                : lab::add(subtract(_f->generator, first.y, _f->generator), second.y, _f->generator); //y2-y1
 
-			tmp1 = multiply(m, m, _f->generator);//= m^2
-            //std::cout << tmp1 << std::endl;
+        tmp2 = second.x >= first.x ? 
+                subtract(second.x, first.x, _f->generator) 
+                : lab::add(subtract(_f->generator, first.x, _f->generator), second.x, _f->generator); //x2-x1
+        } else {      
+            tmp1 = multiply(first.x, first.x, _f->generator); //x1^2
+            tmp1 = multiply(3_bn, tmp1, _f->generator); //3*x1^2
+            tmp1 = lab::add(tmp1, _a, _f->generator);//3*x1^2 + A
+            tmp2 = multiply(2_bn, first.y, _f->generator);//2y1
+        }
+        m = multiply(tmp1,lab::inverted(tmp2,_f->generator, BigNum::InversionPolicy::Fermat),_f->generator); //tmp1 / tmp2
 
-			tmp2 = lab::add(first.x, second.x, _f->generator); //= x1+x2
-            //std::cout << tmp2 << std::endl;
+        tmp1 = multiply(m, m, _f->generator);//= m^2
+
+        tmp2 = lab::add(first.x, second.x, _f->generator); //= x1+x2
 			
-			tmp1 = tmp1 >= tmp2 ?
-				subtract(tmp1, tmp2, _f->generator)
-				: lab::add(subtract(_f->generator, tmp2, _f->generator), tmp1, _f->generator); //= x3
-            //std::cout << tmp1 << std::endl;
+        tmp1 = tmp1 >= tmp2 ?
+                subtract(tmp1, tmp2, _f->generator)
+                : lab::add(subtract(_f->generator, tmp2, _f->generator), tmp1, _f->generator); //= x3
 
-			tmp2 = first.x >= tmp1 ? 
-				subtract(first.x, tmp1, _f->generator) 
-				: lab::add(subtract(_f->generator, tmp1, _f->generator), first.x, _f->generator); //= x1-x3			
-            //std::cout << tmp2 << std::endl;
+        tmp2 = first.x >= tmp1 ? 
+                subtract(first.x, tmp1, _f->generator) 
+                : lab::add(subtract(_f->generator, tmp1, _f->generator), first.x, _f->generator); //= x1-x3			
 
-			tmp2 = multiply(m, tmp2, _f->generator); //= m*(x1-x3)
-            //std::cout << tmp2 << std::endl;
+        tmp2 = multiply(m, tmp2, _f->generator); //= m*(x1-x3)
 
-            tmp2 = tmp2 >= first.y ?
+        tmp2 = tmp2 >= first.y ?
                 subtract(tmp2, first.y, _f->generator)
                 : lab::add(subtract(_f->generator, first.y, _f->generator), tmp2, _f->generator); //= y3
-            //std::cout << tmp2 << std::endl;
 
-			return{ tmp1,tmp2 }; //{x3,y3} - answer
-		}
-	}
+        return{ tmp1,tmp2 }; //{x3,y3} - answer
+    }
+}
 }
--- Src/EllipticCurves.hpp
+++ Src/EllipticCurves.hpp
@@ -5,72 +5,73 @@
 
 
 namespace lab {
-	struct Point {
-		BigNum x;
-		BigNum y;
 
-        Point(const BigNum& X,const BigNum& Y):x(X),y(Y){}
-		friend bool operator==(const Point& left, const Point& right) {
-			return (left.x == right.x) && (left.y == right.y);
-		}
-	};
+struct Point {
+    BigNum x;
+    BigNum y;
 
-	struct Field {
-		BigNum generator;
-		Field(const BigNum& g):generator(g){}
+    Point(const BigNum& X, const BigNum& Y) :x(X), y(Y) {}
+    friend bool operator==(const Point& left, const Point& right) {
+        return (left.x == right.x) && (left.y == right.y);
+    }
+};
 
-		friend bool operator==(const Field& left, const Field& right) {
-			return left.generator == right.generator;
-		}		
-	};
+struct Field {
+    BigNum generator;
+    Field(const BigNum& g) :generator(g) {}
 
-	class EllipticCurve {
-	public:
-		EllipticCurve(const EllipticCurve& that) = default;
-		
-		EllipticCurve(Field* f, const BigNum& a, const BigNum& b);
-		
-		EllipticCurve() = default;
+    friend bool operator==(const Field& left, const Field& right) {
+        return left.generator == right.generator;
+    }
+};
 
-		EllipticCurve& operator=(const EllipticCurve& that) = default;
+class EllipticCurve {
+public:
+    EllipticCurve(const EllipticCurve& that) = default;
 
-        static inline const Point neutral = { BigNum::inf(), BigNum::inf() };
+    EllipticCurve(Field* f, const BigNum& a, const BigNum& b);
 
-		friend bool operator==(const EllipticCurve& left, const EllipticCurve& right);
-		friend bool operator!=(const EllipticCurve& left, const EllipticCurve& right);
+    EllipticCurve() = default;
 
-		template<typename OStream>
-		friend OStream& operator<<(OStream& os, const EllipticCurve& curve);
+    EllipticCurve& operator=(const EllipticCurve& that) = default;
 
-		/**
-		* @return True if elliptic curve contains given point, otherwise false
-		*/
-		bool contains(const Point& p) const;
+    static inline const Point neutral = { BigNum::inf(), BigNum::inf() };
 
-		/**
-		* @brief Looks for points on curve with given x-coordinate
-		* @return Vector of matching points
-		*/
-		std::vector<Point> findPoints(const BigNum& x);
+    friend bool operator==(const EllipticCurve& left, const EllipticCurve& right);
+    friend bool operator!=(const EllipticCurve& left, const EllipticCurve& right);
 
-		/**
-		* @brief p must belong to curve
-		*/
-		Point inverted(const Point& p);
+    template<typename OStream>
+    friend OStream& operator<<(OStream& os, const EllipticCurve& curve);
 
-		Point add(const Point& first, const Point& second);
+    /**
+    * @return True if elliptic curve contains given point, otherwise false
+    */
+    bool contains(const Point& p) const;
 
-	private:
-		///< y^2 = x^3 + a*x + b on field f
-		Field* _f;
-		BigNum _a;
-		BigNum _b;
-	};
+    /**
+    * @brief Looks for points on curve with given x-coordinate
+    * @return Vector of matching points
+    */
+    std::vector<Point> findPoints(const BigNum& x);
 
-	template<typename OStream>
-	OStream& operator<<(OStream& os, const EllipticCurve& curve) {
-        os << "y^2 = x^3 + " << curve._a << "*x + " << curve._b << " mod " << curve._f->generator;
-		return os;
-	}
+    /**
+    * @brief p must belong to curve
+    */
+    Point inverted(const Point& p);
+
+    Point add(const Point& first, const Point& second);
+
+   private:
+    ///< y^2 = x^3 + a*x + b on field f
+    Field* _f;
+    BigNum _a;
+    BigNum _b;
+};
+
+template<typename OStream>
+OStream& operator<<(OStream& os, const EllipticCurve& curve) {
+    os << "y^2 = x^3 + " << curve._a << "*x + " << curve._b << " mod " << curve._f->generator;
+    return os;
+}
 
 } // namespace lab
