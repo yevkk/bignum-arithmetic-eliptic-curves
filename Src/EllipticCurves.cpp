0ee9def670a8033d8c993c93fa0035f539dcf514
SophyaTs <56018862+SophyaTs@users.noreply.github.com>
2019-12-02 23:13:56 +0200

    Elliptic curves (#10)
    * Test commit with first progress
    
    * Update
    
    * Base class has functioning methods, tests added
    
    * Code appereance improved, added minor change in fucntion EllipticCurves::add
    
    * Trying to fix vector issue
    
    * Inverted and Add are const now

--- Src/EllipticCurves.cpp
+++ Src/EllipticCurves.cpp
@@ -1,1 +1,76 @@
 #include <EllipticCurves.hpp>
+
+namespace lab {
+
+EllipticCurve::EllipticCurve(Field* f, const BigNum& a, const BigNum& b): _f(f),_a(a),_b(b){}
+
+bool operator==(const EllipticCurve& left, const EllipticCurve& right) {
+    return (*left._f == *right._f) && (left._a == right._a) && (left._b == right._b);
+}
+
+bool operator!=(const EllipticCurve& left, const EllipticCurve& right) {
+    return !(left == right);
+}
+
+bool EllipticCurve::contains(const Point& p) const {
+    if (multiply(p.y, p.y, _f->generator)
+           == lab::add(multiply(multiply(p.x, p.x, _f->generator), p.x, _f->generator), lab::add(multiply(_a, p.x, _f->generator), _b, _f->generator), _f->generator))
+       return true;
+    else 
+       return false;
+ }
+
+Point EllipticCurve::inverted(const Point& p) const {
+    return { p.x,_f->generator - p.y };
+}
+
+Point EllipticCurve::add(const Point& first, const Point& second) const {
+    if (first == neutral || second == neutral)
+        return first == neutral ? second : first;
+    
+    if ((first.x == second.x && first.y != second.y)
+        || (first == second && first.y == 0_bn))
+    {
+        return neutral;
+    } else {
+        BigNum tmp1;
+        BigNum tmp2;
+        BigNum m;
+        if (first.x != second.x) {
+        tmp1 = second.y >= first.y ? 
+                subtract(second.y, first.y, _f->generator) 
+                : lab::add(subtract(_f->generator, first.y, _f->generator), second.y, _f->generator); //y2-y1
+
+        tmp2 = second.x >= first.x ? 
+                subtract(second.x, first.x, _f->generator) 
+                : lab::add(subtract(_f->generator, first.x, _f->generator), second.x, _f->generator); //x2-x1
+        } else {      
+            tmp1 = multiply(first.x, first.x, _f->generator); //x1^2
+            tmp1 = multiply(3_bn, tmp1, _f->generator); //3*x1^2
+            tmp1 = lab::add(tmp1, _a, _f->generator);//3*x1^2 + A
+            tmp2 = multiply(2_bn, first.y, _f->generator);//2y1
+        }
+        m = multiply(tmp1,lab::inverted(tmp2,_f->generator, BigNum::InversionPolicy::Fermat),_f->generator); //tmp1 / tmp2
+
+        tmp1 = multiply(m, m, _f->generator);//= m^2
+
+        tmp2 = lab::add(first.x, second.x, _f->generator); //= x1+x2
+			
+        tmp1 = tmp1 >= tmp2 ?
+                subtract(tmp1, tmp2, _f->generator)
+                : lab::add(subtract(_f->generator, tmp2, _f->generator), tmp1, _f->generator); //= x3
+
+        tmp2 = first.x >= tmp1 ? 
+                subtract(first.x, tmp1, _f->generator) 
+                : lab::add(subtract(_f->generator, tmp1, _f->generator), first.x, _f->generator); //= x1-x3			
+
+        tmp2 = multiply(m, tmp2, _f->generator); //= m*(x1-x3)
+
+        tmp2 = tmp2 >= first.y ?
+                subtract(tmp2, first.y, _f->generator)
+                : lab::add(subtract(_f->generator, first.y, _f->generator), tmp2, _f->generator); //= y3
+
+        return{ tmp1,tmp2 }; //{x3,y3} - answer
+    }
+}
+}
---
+++ Src/PredefineEllipticCurves.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "EllipticCurves.hpp"
+#include <array>
+
+namespace lab {
+
+struct FieldMeta {
+    Field field;
+    static constexpr int CURVES_PER_FIELED = 3;
+    std::array<EllipticCurve, CURVES_PER_FIELED>  curves;
+};
+    
+/*
+* @brief Elliptic curves database with thier labels on https://www.lmfdb.org/EllipticCurve/Q/
+*        (paste label into a box near the big blue button)
+*/
+static const int FIELD_NUMBER = 3;
+static const FieldMeta curveDataBase[] = { 
+    {Field(234131_bn),
+        {EllipticCurve(new Field(234131_bn),228960_bn,91781_bn), //100016.g1
+         EllipticCurve(new Field(234131_bn),3133_bn,46606_bn), //100040.g1
+         EllipticCurve(new Field(234131_bn),227064_bn,9977_bn) //100040.g4
+        } 
+    }, 
+    {Field(80000005213_bn),
+        {EllipticCurve(new Field(80000005213_bn),39912548_bn,7610314_bn), //100016.h1
+         EllipticCurve(new Field(80000005213_bn),39796616_bn,38003178_bn), //100040.d2
+         EllipticCurve(new Field(80000005213_bn),79966658546_bn,74118524074_bn) //200080.q1
+        } 
+    },
+    {Field(773_bn),
+        {EllipticCurve(new Field(773_bn),761_bn,65_bn), //252.a2
+         EllipticCurve(new Field(773_bn),446_bn,724_bn), //252.a1
+         EllipticCurve(new Field(773_bn),7361_bn,20_bn) //216.a1
+        }
+    }
+};
+} // namespace lab
