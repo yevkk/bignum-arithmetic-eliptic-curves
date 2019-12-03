663dfb9954004b5e0f445d9b71358ab94d19dbf8
carexoid <55104791+carexoid@users.noreply.github.com>
2019-12-03 09:40:00 +0200

    Elliptic curves (#1)
    * Test commit with first progress
    
    * Update
    
    * Base class has functioning methods, tests added
    
    * Code appereance improved, added minor change in fucntion EllipticCurves::add
    
    * Trying to fix vector issue
    
    * Inverted and Add are const now
    
    * Hotfix for namespaces

--- Tests/TestEllipticCurves.cpp
+++ Tests/TestEllipticCurves.cpp
@@ -34,11 +34,11 @@ TEST_CASE("Elliptic curves test", "[curves]") {
         SECTION("Regular point") {
             lab::Point p = { 535_bn, 12444_bn };
             lab::Point expected = { 535_bn, 221687_bn };
-            REQUIRE(expected == curveDataBase[0].curves[2].inverted(p));
+            REQUIRE(expected == curveDataBase[0].curves[2].invertedPoint(p));
         }
         SECTION("Point with y = 0") {
             lab::Point p = { 234131_bn, 0_bn };
-            REQUIRE(p == curveDataBase[0].curves[2].inverted(p));
+            REQUIRE(p == curveDataBase[0].curves[2].invertedPoint(p));
         }
     }
     
@@ -47,13 +47,13 @@ TEST_CASE("Elliptic curves test", "[curves]") {
             lab::Point p1 = { 769_bn, 7_bn };
             lab::Point p2 = {70_bn,585_bn};
             lab::Point expected = { 519_bn, 149_bn};
-            REQUIRE(expected == curveDataBase[2].curves[0].add(p1,p2));
+            REQUIRE(expected == curveDataBase[2].curves[0].addPoints(p1,p2));
         }
         SECTION("Normal") {
             lab::Point p1 = { 2570_bn, 130216_bn};
             lab::Point p2 = { 110_bn,574_bn };
             lab::Point expected = { 96091_bn, 21870_bn };
-            REQUIRE(expected == curveDataBase[0].curves[2].add(p1, p2));
+            REQUIRE(expected == curveDataBase[0].curves[2].addPoints(p1, p2));
         }
         //TODO
         SECTION("Challenging") {
@@ -62,7 +62,7 @@ TEST_CASE("Elliptic curves test", "[curves]") {
         SECTION("Neutral point") {
             lab::Point p1 = {89_bn,320_bn};
             lab::Point p2 = { 89_bn,233811_bn };
-            REQUIRE(lab::EllipticCurve::neutral == curveDataBase[0].curves[0].add(p1, p2));
+            REQUIRE(lab::EllipticCurve::neutral == curveDataBase[0].curves[0].addPoints(p1, p2));
         }
     }
 }
